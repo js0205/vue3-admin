7b8c94f254f0a1ee7b505143f4300c5f4e97b7eb
js0205 <467353069@qq.com>
2022-11-04 22:31:00 +0800

    feat: 登录接口交互

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>后台首页</div>
+  <div>
+    后台首页
+    <el-button @click="addCount">{{ count }}</el-button>
+    <el-button type="primary" @click="addCount2">{{ form.count }}</el-button>
+  </div>
 </template>
+<script setup>
+import { ref, reactive } from "vue";
+let count = ref(1);
+function addCount() {
+  console.log("addCount");
+  count.value++;
+  console.log(count.value);
+}
+const form = reactive({
+  count: 2,
+});
+function addCount2() {
+  form.count++;
+  console.log(form.count);
+}
+</script>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,31 +1,34 @@
 <template>
-  <el-row style="min-height: 100vh" class="bg-indigo-500">
-    <el-col :lg="16" :md="12" class="flex items-center justify-center">
+  <el-row class="login-container">
+    <el-col :lg="16" :md="12" class="left">
       <div>
-        <div class="font-bold text-5xl text-light-50 mb-4">欢迎光临</div>
-        <div class="text-gray-200 text-sm">
-          此站点是《Vue3实战商城后台管理系统开发》的演示地址
-        </div>
+        <div>欢迎光临</div>
+        <div>此站点是《Vue3实战商城后台管理系统开发》的演示地址</div>
       </div></el-col
     >
-    <el-col
-      :lg="8"
-      :md="12"
-      class="bg-light-50 flex items-center justify-center flex-col"
-      ><h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
-      <div
-        class="flex items-center justify-center my-5 text-gray-300 space-x-2"
-      >
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+    <el-col :lg="8" :md="12" class="right"
+      ><h2 class="title">欢迎回来</h2>
+      <div>
+        <span class="line"></span>
         <span>账号密码登录</span>
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+        <span class="line"></span>
       </div>
-      <el-form :model="form" class="w-[250px]">
-        <el-form-item>
-          <el-input v-model="form.username" placeholder="请输入用户名" />
+      <el-form ref="formRef" :rules="rules" :model="form" class="w-[250px]">
+        <el-form-item prop="username">
+          <el-input v-model="form.username" placeholder="请输入用户名"
+            ><template #prefix>
+              <el-icon><user /></el-icon> </template
+          ></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input v-model="form.password" placeholder="请输入密码" />
+        <el-form-item prop="password">
+          <el-input
+            v-model="form.password"
+            placeholder="请输入密码"
+            type="password"
+            show-password
+            ><template #prefix>
+              <el-icon><lock /></el-icon> </template
+          ></el-input>
         </el-form-item>
         <el-form-item>
           <el-button
@@ -42,15 +45,87 @@
   </el-row>
 </template>
 <script setup>
-import { reactive } from "vue";
+import { ref, reactive } from "vue";
+import { ElNotification } from "element-plus";
+import { useRouter } from "vue-router";
+import { login } from "~/api/manager";
 
+const router = useRouter();
 // do not use same name with ref
 const form = reactive({
   username: "",
   password: "",
 });
-
+const rules = {
+  username: [
+    {
+      required: true,
+      message: "用户名不能为空",
+      trigger: "blur",
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "密码不能为空",
+      trigger: "blur",
+    },
+  ],
+};
+const formRef = ref(null);
 const onSubmit = () => {
-  console.log("submit!");
+  formRef.value.validate((valid) => {
+    if (!valid) {
+      return false;
+    }
+    login(form.username, form.password)
+      .then((res) => {
+        console.log(res.data.data);
+        //提示成功
+        ElNotification({
+          message: "登陆成功",
+          type: "success",
+          duration: 1000,
+        });
+        //存储token和用户相关信息
+
+        //跳转到后台首页
+        router.push("/");
+      })
+      .catch((err) => {
+        ElNotification({
+          message: err.response.data.msg || "请求失败",
+          type: "error",
+          duration: 1000,
+        });
+      });
+  });
 };
 </script>
+<style>
+.login-container {
+  @apply min-h-screen bg-indigo-500;
+}
+.login-container .left,
+.login-container .right {
+  @apply flex items-center justify-center;
+}
+.login-container .right {
+  @apply bg-light-50 flex-col;
+}
+.left > div > div:first-child {
+  @apply font-bold text-5xl text-light-50 mb-4;
+}
+.left > div > div:last-child {
+  @apply text-gray-200 text-sm;
+}
+.right .title {
+  @apply font-bold text-3xl text-gray-800;
+}
+.right > div {
+  @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
+}
+.right .line {
+  @apply h-[1px] w-16 bg-gray-200;
+}
+</style>
